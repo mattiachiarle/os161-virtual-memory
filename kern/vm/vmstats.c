863a2cf250d985c2778fbf507ca9bf22dbc5d4a6
Francesca Fusco <s310039@studenti.polito.it>
2023-07-03 21:12:43 +0000

    implemented utility functions for statistics

--- kern/vm/vmstats.c
+++ kern/vm/vmstats.c
@@ -1,1 +1,46 @@
 #include "vmstats.h"
+void init_stat(void){
+    spinlock_init(&stat.lock);
+    stat.tlb_faults=0;
+    stat.faults=0;
+    stat.tlb_free_faults=0;
+    stat.tlb_replace_faults = 0;
+    stat.tlb_invalidations=0;
+    stat.tlb_reloads=0;
+    /*It might be necessary to insert additional initializations, for "temporary results"*/
+}
+
+
+
+/*UTILITY FUNCTIONS*/
+void add_tlb_fault(void){
+    spinlock_acquire(&stat.lock);
+    stat.tlb_faults++;
+    spinlock_release(&stat.lock);
+}
+void add_tlb_type_fault(int type){
+    spinlock_acquire(&stat.lock);
+    switch (type)
+    {
+    case FAULT_W_FREE:
+        stat.tlb_free_faults++;
+        break;
+    case FAULT_W_REPLACE:
+        stat.tlb_replace_faults++;
+        break;
+    default:
+        break;
+    }
+    spinlock_release(&stat.lock);
+}
+void add_tlb_invalidation(void){
+    spinlock_acquire(&stat.lock);
+    stat.tlb_invalidations++;
+    spinlock_release(&stat.lock);
+}
+
+void add_tlb_reload(void){
+    spinlock_acquire(&stat.lock);
+    stat.tlb_reloads++;
+    spinlock_release(&stat.lock);
+}
