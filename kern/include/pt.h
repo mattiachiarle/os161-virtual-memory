7de5638dcf9ce5ea75fb9eb86f2d7921b0c8e569
Giuseppe Gabriele <s310046@studenti.polito.it>
2023-07-05 15:45:09 +0000

    added some funcs and fixes in page table

--- kern/include/pt.h
+++ kern/include/pt.h
@@ -2,33 +2,56 @@
 
 /*
  * Data structure to handle the page table
- * 
+ *
  */
-struct pt_entry{  //this is an entry of our IPT
-    vaddr_t page;  //virt page in the frame
-    pid_t pid;    // processID 
-    uint8_t ctl;  //some bits for control; from the lower:  Validity bit, Reference bit, isInTLB bit, ...
-                    // could be added other bits
-}entr;
+struct pt_entry
+{                 // this is an entry of our IPT
+    vaddr_t page; // virt page in the frame
+    pid_t pid;    // processID
+    uint8_t ctl;  // some bits for control; from the lower:  Validity bit, Reference bit, isInTLB bit, ...
+                 //  could be added other bits
+} entr;
 
-struct pt_entry *pt;  //our IPT
-int ptSize;   //IPT size, in number of pte
+// in future  and add lock. 
+struct ptInfo
+{
+    struct pt_entry *pt; // our IPT
+    int ptSize;          // IPT size, in number of pte
+    paddr_t firstfreepaddr;
+} tommaso;
 
 /*
-* PT INIT
-*/
-void pt_init(void);
+ * PT INIT
+ */
+void
+pt_init(void);
 
 /*
  * This function converts a logical address into a physical one.
  *
  * @param: virtual address that we want to access
  * @param: pid of the process that asks for the page
- * 
- * @return: NULL if the requested page isn't stored in the page table, 
+ *
+ * @return: NULL if the requested page isn't stored in the page table,
  * physical address otherwise
  */
-paddr_t pt_get_paddr(vaddr_t,pid_t);
+
+paddr_t pt_get_paddr(vaddr_t, pid_t);
+
+/*
+ * find the cur PID and calls getpaddr that returns something.
+ * IF pt_get_paddr is !== -1 return the paddr
+ * ELSE calls function findspace() that finds a free space in IPT.
+ *      IF found calls LOAD_PAGE Mattia's function
+ *      ELSE calls find_victim that remove an entry and then LOAD_PAGE Mattia's function
+ */
+paddr_t get_page(vaddr_t);
+
+/*
+        wrapper for getpaddr
+        it receives only vaddr and it takes pid from curproc
+        if returns NULL call mattia's functions else return paddr
+*/
 
 /*
  * This function loads a new page from the swapfile or the elf file
@@ -38,16 +61,21 @@ paddr_t pt_get_paddr(vaddr_t,pid_t);
  *
  * @param: virtual address that we want to access
  * @param: pid of the process that asks for the page
- * 
+ *
  * @return: NULL in case of errors, physical address otherwise
  */
-paddr_t load_page(vaddr_t,pid_t);
+paddr_t load_page(vaddr_t, pid_t);
+
+paddr_t find_victim();
 
 /*
  * This function frees all the pages of a process after its termination.
  *
  * @param: pid of the ended process
- * 
+ *
  * @return: -1 iin case of errors, 0 otherwise
  */
 int free_pages(pid_t);
+
+
+int cabodi(vaddr_t);
--- kern/vm/pt.c
+++ kern/vm/pt.c
@@ -8,6 +8,7 @@
 #include "pt.h"
 
 int lastIndex = 0; // for FIFO, need to check priorities: Ref. bit and TLB bit
+// for now idea: to get first addr use ram_stealmem or put visible firstaddr
 
 void pt_init(void)
 {
@@ -16,114 +17,135 @@ void pt_init(void)
     // modify in order to take only locatable free space
     numFrames = mainbus_ramsize() / PAGE_SIZE; // get how many frames I have in RAM, remember: 1 IPT entry for each frame
     // the next one: how many entries in a frame, that is a ceil
-    entInFrame = PAGE_SIZE / sizeof(struct pt_entry) + ((PAGE_SIZE % sizeof(struct pt_entry)) != 0);
     // the next one: number of frames for the page table = num of frames / how many entries in a frame, with ceil
-    framesForPT = numFrames / (entInFrame) + ((numFrames % sizeof(struct pt_entry)) != 0);
     // allocate IPT in kern
-    pt = kmalloc(sizeof(struct pt_entry) * numFrames);
-    if (pt == NULL) //
+    tommaso.pt = kmalloc(sizeof(struct pt_entry) * numFrames);
+    if (tommaso.pt == NULL) //
     {
         panic("error allocating IPT");
     }
-    for (int i = 0; i < numFrames; i++)
+    for (int i = 0; i < numFrames; i++) // all validity bit to 0
     {
-        pt[i].ctl = pt[i].ctl & ~1;
+        tommaso.pt[i].ctl = tommaso.pt[i].ctl & ~1;
     }
-    ptSize = numFrames;
+    tommaso.ptSize = numFrames;
+    tommaso.firstfreepaddr = ram_stealmem(0);
 }
 
 // some bits for control; from the lower:  Validity bit, Reference bit, isInTLB bit, ...
 //  bitStatus = (j >> n) & 1;
 
+paddr_t get_page(vaddr_t v)
+{
+    pid_t pid; // get curpid here
+    paddr_t pp;
+    pp = pt_get_paddr(v, pid);
+
+    if (pp != -1)
+    {
+        return pp;
+    }
+    int pos = findspace(); // find a free space
+    if (pos == -1)
+    {
+        // find victim
+    }
+
+    // MATTIA LOAD_PAGE( v, pid, pos*PAGE_SIZE );
+}
+
+int findspace()
+{
+    int validity = -1;
+    for (int i = 0; i < tommaso.ptSize; i++)
+    {
+        validity = tommaso.pt[i].ctl & 1; // 1 if validity bit=1
+        if (!validity)
+        {
+            return i; // return the position of empty entry in PT
+        }
+    }
+    return -1;
+}
+
 paddr_t pt_get_paddr(vaddr_t v, pid_t p)
 {
     int validity;
 
-    for (int i = 0; i < ptSize; i++)
+    for (int i = 0; i < tommaso.ptSize; i++)
     {
-        validity = pt[i].ctl & 1;
-        if (validity && v == pt[i].page && pt[i].pid == p)
+        validity = tommaso.pt[i].ctl & 1;
+        if (v == tommaso.pt[i].page && tommaso.pt[i].pid == p && validity)
         {
-            pt[i].ctl = pt[i].ctl | 2; // set RB to 1, that is the second ctl bit
-            // insert in TLB DONT KNOW HOW
-            pt[i].ctl = pt[i].ctl | 4;              // set isInTLB to 1, third ctl bit
-            return i * PAGE_SIZE /*+ firstpaddr */; // send the paddr found
+            tommaso.pt[i].ctl = tommaso.pt[i].ctl | 2;     // set RB to 1, that is the second ctl bit
+            tommaso.pt[i].ctl = tommaso.pt[i].ctl | 4;     // set isInTLB to 1, third ctl bit
+            return i * PAGE_SIZE + tommaso.firstfreepaddr; // send the paddr found
         }
     }
+    // else insert in IPT the right varrd+pid from Mattia el Chiurlo
 
-    return NULL; // not found in PT
+    return -1; // not found in PT in future PANIC!!
 }
 
-paddr_t load_page(vaddr_t v, pid_t p)
+// name is wrong --> select victim
+paddr_t find_victim()
 {
-    KASSERT(is_bitmap_active);
-
     int i;
-    for (i = 0; i < lastIndex + 1; i = (i + 1) % ptSize)
-    {
-        if ((pt[i].ctl & 1) == 0) // if the entry is invalid I can use it
-        {
-            pt[i].ctl |= 1; // set Validity bit to 1
-            //
-            // insert in TLB
-            //
-            pt[i].ctl |= 4; // set isInTLB bit
-            pt[i].page = v; // update vaddr and pageid inside page table
-            pt[i].page = p;
-            // if present in swapfile remove it from there
-            lastIndex = i; // update last index for FIFO
-            return i * PAGE_SIZE /* + firstpaddr */;
-        }
-    }
     // if I am here there will be a replacement since all pages are valid
-
-    for (i = lastIndex; i < lastIndex + 1; i = (i + 1) % ptSize)
+    for (i = lastIndex;; i = (i + 1) % tommaso.ptSize)
     {
-        if ((pt[i].ctl & 4) == 0) // if RB = 0
+        // Val, Ref,TLB bits from low to high
+        if ((tommaso.pt[i].ctl & 4) == 0) // if isInTLB = 0
         {
-            // remove old one from TLB and insert the new one
-            // Put in swapfile old pt[i].v and old pt[i].v
-            pt[i].ctl |= 4; // set isInTLB bit
-            pt[i].page = v; // update vaddr and pageid inside page table
-            pt[i].page = p;
-            lastIndex = i; // update last index for FIFO
-            return i * PAGE_SIZE /* + firstpaddr */;
-        }
-    }
-
-    for (i = lastIndex; i < lastIndex + 1; i = (i + 1) % ptSize)
-    {
-        if ((pt[i].ctl & 2) == 0) // if RB = 0
-        {
-            pt[i].ctl |= 2; // set RB bit to 1
-            //
-            // remove the old one from TLB and insert the new one
-            //
-            // Put in swapfile old pt[i].v and old pt[i].v
-            pt[i].ctl |= 4; // set isInTLB bit
-            pt[i].page = v; // update vaddr and pageid inside page table
-            pt[i].page = p;
-            lastIndex = i; // update last index for FIFO
-            return i * PAGE_SIZE /* + firstpaddr */;
-        }
-        else
-        {
-            pt[i].ctl = pt[lastIndex].ctl & ~2; // set RB to 0 if not here
+            if (tommaso.pt[i].ctl & 2 == 0) // if Ref bit==0 victim found
+            {
+                tommaso.pt[i].ctl |= 4;                        // set isINTLB to 1
+                lastIndex = i + 1;                             // new ptr for FIFO
+                return i * PAGE_SIZE + tommaso.firstfreepaddr; // return paddr of that frame
+            }
+            else
+            {                                               // RB=1
+                tommaso.pt[i].ctl = tommaso.pt[i].ctl & ~2; // set RB to 0 and continue
+            }
         }
     }
 }
+
+// remove old one from TLB and insert the new one
+// Put in swapfile old pt[i].v and old pt[i].v
+// pt[i].ctl |= 4; // set isInTLB bit
+// pt[i].page = v; // update vaddr and pageid inside page table
+// pt[i].page = p;
+// lastIndex = i; // update last index for FIFO
+// return i * PAGE_SIZE /* + firstpaddr */;
 
 int free_pages(pid_t p)
 {
 
-    for (int i = 0; i < ptSize; i++)
+    for (int i = 0; i < tommaso.ptSize; i++)
     {
-        if (pt[i].pid == p && (pt[i].ctl | 1) == 1) // if it is valid and it is the same pid passed
+        if (tommaso.pt[i].pid == p && (tommaso.pt[i].ctl | 1) == 1) // if it is valid and it is the same pid passed
         {
-            pt[i].ctl &= 0;  //set validity bit = 0
+            tommaso.pt[i].ctl &= 0; // set all controls to 0
         }
     }
 
-    //free pages also in swapfile
+    // free pages also in swapfile
+}
 
+int cabodi(vaddr_t v)
+{
+    pid_t pid; // take pid from curproc
+    int i;
+    for (i = 0; i < tommaso.ptSize; i++)
+    {
+        if (tommaso.pt[i].page == v)
+        {
+            KASSERT( (tommaso.pt[i].ctl & 4 ) );  //it must be inside TLB
+            tommaso.pt[i].ctl = tommaso.pt[i].ctl & ~ 4; //remove TLB bit
+            tommaso.pt[i].ctl |= 2;  // set RB to 1
+            return 1; // 
+        }
+    }
+    return -1;
 }
