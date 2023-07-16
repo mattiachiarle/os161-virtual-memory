debcbc2cb4db43c2c786cf1fe6c179f339706c0d
Giuseppe Gabriele <s310046@studenti.polito.it>
2023-07-16 09:15:30 +0000

    added comments in pt.h

--- kern/include/pt.h
+++ kern/include/pt.h
@@ -10,7 +10,7 @@
 
 int pt_active;
 #if OPT_DEBUG
-int nkmalloc; //nkmalloc = number of pages allocated with kmalloc - number of pages freed
+int nkmalloc; // nkmalloc = number of pages allocated with kmalloc - number of pages freed
 #endif
 
 /*
@@ -26,108 +26,195 @@ struct pt_entry
 
 struct ptInfo
 {
-    struct pt_entry *pt; // our IPT
-    int ptSize;          // IPT size, in number of pte
-    paddr_t firstfreepaddr; //Offset to use to compute the physical address of the frames
-    struct lock *pt_lock; //Necessary for the cv
-    struct cv *pt_cv; //Used to sleep if the IPT is full
-    int *contiguous; //Used to keep track of how many pages we need to free
+    struct pt_entry *pt;    // our IPT
+    int ptSize;             // IPT size, in number of pte
+    paddr_t firstfreepaddr; // Offset to use to compute the physical address of the frames
+    struct lock *pt_lock;   // Necessary for the cv
+    struct cv *pt_cv;       // Used to sleep if the IPT is full
+    int *contiguous;        // Used to keep track of how many pages we need to free
 } peps;
 
-struct hashentry  // single entry of hashtable
+struct hashentry // single entry of hashtable
 {
-    int iptentry;   // "ptr" to IPT entry
-    vaddr_t vad; //virtual address of the entry
-    pid_t pid; //pid of the entry
-    struct hashentry *next;  //ptr to next hashentry
+    int iptentry;           // "ptr" to IPT entry
+    vaddr_t vad;            // virtual address of the entry
+    pid_t pid;              // pid of the entry
+    struct hashentry *next; // ptr to next hashentry
 };
 
-struct hashT   // struct   
+struct hashT // struct
 {
-    struct hashentry **table;   // array of list of hashentry with dimension size.
-    int size; // 2 times the IPT
+    struct hashentry **table; // array of list of hashentry with dimension size.
+    int size;                 // 2 times the IPT
 } htable;
 
-struct hashentry *unusedptrlist;  // list where all unused blocks for the hast table are stored
+struct hashentry *unusedptrlist; // list where all unused blocks for the hast table are stored
 
 /*
  * PT INIT
  */
 void pt_init(void);
 
-/*
- * This function converts a logical address into a physical one.
+/**
+ * This function gets the physical address from the IPT, then updated with the Hash table
  *
- * @param: virtual address that we want to access
- * @param: pid of the process that asks for the page
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
  *
- * @return: -1 if the requested page isn't stored in the page table,
- * physical address otherwise
+ *
+ * @return -1 if the requested page isn't stored in the page table, physical address otherwise
  */
-
 int pt_get_paddr(vaddr_t, pid_t);
 
-/*  THIS IS THE BIG WRAPPER OF ALL OTHER FUNCS
- * find the cur PID and calls getpaddr that returns something.
- * IF pt_get_paddr is !== -1 return the paddr
- * ELSE calls function findspace() that finds a free space in IPT.
- *      IF found calls LOAD_PAGE Mattia's function
- *      ELSE calls find_victim that remove an entry and then LOAD_PAGE Mattia's function
+/**
+ * This function is a wrapper for the following process:
+ *  if physical frame found return directly the current physical address
+ *  if not found find a position to insert the new element
+ *  if all full find a victim
+ *  return the physical position of the page
+ *
+ * @param vaddr_t: virtual address
+ *
+ *
+ * @return physical address found inside the IPT
  */
 paddr_t get_page(vaddr_t);
 
-/*
-        wrapper for getpaddr
-        it receives only vaddr and it takes pid from curproc
-        if returns NULL call mattia's functions else return paddr
-*/
-
-/*
- * This function loads a new page from the swapfile or the elf file
- * (depending on the virtual address provided). If the page table is full,
- * it selects the page to remove by using second-chance and it stores it in
- * the swapfile.
+/**
+ * This function finds a victim in the IPT and the updates the hash table.
+ *  it uses a second chance algorithm based on TLB presence and reference bit
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
  *
- * @param: virtual address that we want to access
- * @param: pid of the process that asks for the page
  *
- * @return: NULL in case of errors, physical address otherwise
+ * @return physical address found inside the IPT
  */
-// paddr_t load_page(vaddr_t, pid_t);
-
 int find_victim(vaddr_t, pid_t);
 
-/*
- * This function frees all the pages of a process after its termination.
+/**
+ * This function frees all the pages inside the IPT
  *
- * @param: pid of the ended process
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
  *
- * @return: -1 iin case of errors, 0 otherwise
+ * @return void
  */
 void free_pages(pid_t);
 
+/**
+ * This function inserts into the hash table, from unusedptrlist, a page, in order to fastly find the index
+ *
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
+ * @param int: page index inside the IPT
+ *
+ * @return void
+ */
 void add_in_hash(vaddr_t, pid_t, int);
 
+/**
+ * This function advices that a frame(virtual address) is removed from TLB.
+ *
+ * @param vaddr_t: virtual address
+ *
+ *
+ * @return 1 if everything ok, -1 otherwise
+ */
 int update_tlb_bit(vaddr_t, pid_t);
 
+/**
+ * This function inserts in the IPT some kernel memory in a contiguous way
+ *
+ * @param int: the number of pages to allocate
+ *
+ * @return physical address found inside the IPT
+ */
 paddr_t get_contiguous_pages(int);
 
+/**
+ * This function retrieves the correct intex of the IPT from the hash table
+ *
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
+ *
+ *
+ * @return the IPT entry value, -1 otherwise
+ */
 int get_index_from_hash(vaddr_t, pid_t);
 
+/**
+ * This function frees the contiguous pages allocated in IPT
+ *
+ * @param vaddr_t: virtual address
+ *
+ *
+ * @return void
+ */
 void free_contiguous_pages(vaddr_t);
 
+/**
+ * This function is used to copy inside PT or Swap-file all the pages of the old pid, but with a new one
+ *  used for forks
+ *
+ * @param pid_t: old pid to copy from
+ * @param pid_t: new pid to add for each page
+ *
+ *
+ * @return void
+ */
 void copy_pt_entries(pid_t, pid_t);
 
+/**
+ * This function setups some bits before the copy_pt_entries related to a fork
+ *
+ * @param pid_t: pages with that pid will be prepared for SWAP
+ *
+ *
+ * @return void
+ */
 void prepare_copy_pt(pid_t);
 
+/**
+ * This function setups setups to zero all the SWAP bits related to the passed pid
+ *
+ * @param pid_t: pages with this pid will be updated with SWAP-bit=0
+ *
+ *
+ * @return void
+ */
 void end_copy_pt(pid_t);
 
 void print_nkmalloc(void);
 
+/**
+ * This function removes from the hash table a list block and adds it again in unusedptrlist
+ *
+ *
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
+ *
+ *
+ * @return physical address found inside the IPT
+ */
 void remove_from_hash(vaddr_t, pid_t);
 
+/**
+ * This function uses an hash function in order to calculate the entry in the hash table
+ *
+ * @param vaddr_t: virtual address
+ * @param pid_t: pid of the process
+ *
+ *
+ * @return the entry in the hash table
+ */
 int get_hash_func(vaddr_t, pid_t);
 
+/**
+ * This function inits the hash table.
+ *
+ *
+ * @return void
+ */
 void htable_init(void);
 
 #endif
